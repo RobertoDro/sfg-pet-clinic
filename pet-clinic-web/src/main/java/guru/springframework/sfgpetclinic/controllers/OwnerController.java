14a707a81504088ded9bcdafdd40d88103e7dabc
RobertoDro <37771467+RobertoDro@users.noreply.github.com>
2021-02-06 17:55:14 +0200

    display owner record  Closes #55

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,15 +3,18 @@ package guru.springframework.sfgpetclinic.controllers;
 import guru.springframework.sfgpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
-public class OwnerControler {
+public class OwnerController {
 
     private final OwnerService ownerService;
 
-    public OwnerControler(OwnerService ownerService) {
+    public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
 
@@ -23,10 +26,14 @@ public class OwnerControler {
         return "owners/index";
     }
 
-    @RequestMapping("/find")
-    public String findOwners(){
-        return "notimplemented";
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownnerId ){
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownnerId));
+        return mav;
+
     }
+
 
 
 }
--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControlerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControlerTest.java
@@ -29,7 +29,7 @@ class OwnerControlerTest {
     OwnerService ownerService;
 
     @InjectMocks
-    OwnerControler controler;
+    OwnerController controler;
 
     Set<Owner> owners;
 
