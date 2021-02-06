1b9f7b18c2dd9125fe95cafec04776c5beb0e819
RobertoDro <37771467+RobertoDro@users.noreply.github.com>
2021-02-06 18:04:50 +0200

    fix display owner Closes #65

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -18,8 +18,8 @@ public class PetType extends BaseEntity{
     @Column(name = "name")
     private String name;
 
-    public String getName() {
+    @Override
+    public String toString() {
         return name;
     }
-
 }
